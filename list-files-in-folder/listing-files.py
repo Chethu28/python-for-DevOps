35af7490945e216e929138bbcb1ddfb30b4bf091
Chethan Reddy <95994938+Chethu28@users.noreply.github.com>
2023-12-20 11:44:49 +0000

    added code

--- list-files-in-folder/listing-files.py
+++ list-files-in-folder/listing-files.py
@@ -1,1 +1,24 @@
+import os
+def listing_files_in_folder(folder):
+    try:
+        file=os.listdir(folder)
+        print(f"listing the files in folder: {folder}")
+        for files in file:
+            print(files)
+            #continue
+    except FileNotFoundError:
+        print("File not found")
+    except PermissionError:
+        print("No permission to file")
+    
+def main():
+    folders=input("Provide the list of  folders with space: ").split()
+
+    for folder in folders:
+        listing_files_in_folder(folder)
+
+main()
+
+
+    
 
